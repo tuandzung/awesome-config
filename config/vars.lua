b210f028a6815674c5bc70d592062b24db9e7200
Ngo Tuan Dzung <zun1903@gmail.com>
2022-04-21 08:32:17 +0700

    refactor: default tiling layout

--- config/vars.lua
+++ config/vars.lua
@@ -3,8 +3,8 @@ local _M = {}
 local awful = require('awful')
 
 _M.layouts = {
-    awful.layout.suit.floating,
     awful.layout.suit.tile,
+    awful.layout.suit.floating,
     awful.layout.suit.fair,
     awful.layout.suit.fair.horizontal,
     awful.layout.suit.max,
