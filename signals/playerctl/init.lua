80518fc6cc3109f510812ed55bc55ad93d0cfda8
Ngo Tuan Dzung <zun1903@gmail.com>
2022-10-11 17:40:03 +0700

    feat: remove playerctl spotify notification

--- signals/playerctl/init.lua
+++ signals/playerctl/init.lua
@@ -7,21 +7,21 @@ local lain = require("lain")
 
 local markup = lain.util.markup
 
-playerctl:connect_signal("metadata",
-                       function(_, title, artist, album_path, album, new, player_name)
-    if new == true then
-        naughty.notify({title = title, text = artist, image = album_path})
-    end
-end)
+-- playerctl:connect_signal("metadata",
+-- function(_, title, artist, album_path, album, new, player_name)
+-- if new == true then
+-- naughty.notify({title = title, text = artist, image = album_path})
+-- end
+-- end)
 
 -- Get Song Info
 playerctl:connect_signal("metadata",
-                       function(_, title, artist, album_path, album, new, player_name)
-    -- Set art widget
-    -- art:set_image(gears.surface.load_uncached(album_path))
+    function(_, title, artist, album_path, album, new, player_name)
+        -- Set art widget
+        -- art:set_image(gears.surface.load_uncached(album_path))
 
-    -- Set player name, title and artist widgets
-    wibar_widgets.name_widget:set_markup_silently(markup.font(beautiful.font, " " .. player_name .. " "))
-    wibar_widgets.title_widget:set_markup_silently(markup.font(beautiful.font, " " .. title .. " "))
-    wibar_widgets.artist_widget:set_markup_silently(markup.font(beautiful.font, " " .. artist .. " "))
-end)
+        -- Set player name, title and artist widgets
+        wibar_widgets.name_widget:set_markup_silently(markup.font(beautiful.font, " " .. player_name .. " "))
+        wibar_widgets.title_widget:set_markup_silently(markup.font(beautiful.font, " " .. title .. " "))
+        wibar_widgets.artist_widget:set_markup_silently(markup.font(beautiful.font, " " .. artist .. " "))
+    end)
